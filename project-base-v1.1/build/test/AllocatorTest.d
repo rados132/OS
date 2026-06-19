ad9ac970726166f149c60ff4ef5219d461c5af43
Rados Radovanovic <radosradovanovic@gmail.com>
2026-06-19 17:58:18 +0200

    removed build files

--- project-base-v1.1/build/test/AllocatorTest.d
+++
@@ -1,8 +0,0 @@
-build/test/AllocatorTest.o: test/AllocatorTest.cpp test/../lib/hw.h \
- test/../lib/console.h test/../h/MemoryAllocator.hpp
-
-test/../lib/hw.h:
-
-test/../lib/console.h:
-
-test/../h/MemoryAllocator.hpp:
