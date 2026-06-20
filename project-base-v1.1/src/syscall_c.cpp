b34623117fa0a6c5a67fa7f38f9a00466335b234
Rados Radovanovic <radosradovanovic@gmail.com>
2026-06-20 22:43:35 +0200

    some more minor refactors

--- project-base-v1.1/src/syscall_c.cpp
+++ project-base-v1.1/src/syscall_c.cpp
@@ -2,13 +2,13 @@
 
 uint64 sys_call ( uint64 syscall, uint64 arg ) {
 
-    uint64 ret;
+    volatile uint64 ret;
 
     __asm__ volatile (
-        "mv a0, %[a0] \n"
-        "mv a1, %[a1] \n"
-        "ecall          \n"
-        "mv %[ret], a0  \n"
+        "mv a0, %[a0]  \n"
+        "mv a1, %[a1]  \n"
+        "ecall         \n"
+        "mv %[ret], a0 \n"
         : [ret]  "=r"(ret)
         : [a0] "r"(syscall), [a1] "r"(arg)
         : "a0", "a1", "memory"
