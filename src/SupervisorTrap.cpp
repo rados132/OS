f24800ef3312f945d264ae0bf346a2a0a8c2a294
Rados Radovanovic <radosradovanovic@gmail.com>
2026-07-16 02:00:44 +0200

    refactored SupervisorTrap to accomodate riscv.hpp changes

--- src/SupervisorTrap.cpp
+++ src/SupervisorTrap.cpp
@@ -1,6 +1,6 @@
 #include "../lib/hw.h"
 #include "../lib/console.h"
-#include "../inc/RISC_V.hpp"
+#include "../inc/riscv.hpp"
 #include "../inc/syscall_c.hpp"
 #include "../inc/MemoryAllocator.hpp"
 #include "../inc/TCB.hpp"
@@ -9,56 +9,56 @@
 
 extern "C" void supervisor_trap_handler () {
     
-    volatile uint64 sepc     = RISC_V::r_sepc     ();
-    volatile uint64 scause   = RISC_V::r_scause   ();
-    volatile uint64 sstatus  = RISC_V::r_sstatus  ();
+    volatile uint64 sepc    = CSR::r_sepc    ();
+    volatile uint64 scause  = CSR::r_scause  ();
+    volatile uint64 sstatus = CSR::r_sstatus ();
 
     if ( scause == 0x08 || scause == 0x09 ) {
         // ecall from U-mode or S-mode
         
         sepc += 4; // increment pc to point to next instruction after ecall
 
-        uint64 syscall_code = RISC_V::r_user_reg ( A0 );
+        uint64 syscall_code = TrapFrame::r_user_reg ( A0 );
 
         switch ( syscall_code ) {
             case MEM_ALLOC: {
-                size_t size = RISC_V::r_user_reg ( A1 );
+                size_t size = TrapFrame::r_user_reg ( A1 );
                 
                 void*  ptr  = MemoryAllocator::kmalloc ( size );
 
-                RISC_V::w_user_reg ( A0, ( uint64 ) ptr );
+                TrapFrame::w_user_reg ( A0, ( uint64 ) ptr );
                 break;
             }
 
             case MEM_FREE: {
-                void* ptr = (void*) RISC_V::r_user_reg ( A1 );
+                void* ptr = (void*) TrapFrame::r_user_reg ( A1 );
 
                 int   ret = MemoryAllocator::kfree ( ptr );
 
-                RISC_V::w_user_reg ( A0, ( uint64 ) ret );
+                TrapFrame::w_user_reg ( A0, ( uint64 ) ret );
                 break;
             }
 
             case THREAD_CREATE: {
-                thread_t*   handle      = ( thread_t* )   RISC_V::r_user_reg ( A1 );
-                thread_body t_body      = ( thread_body ) RISC_V::r_user_reg ( A2 );
-                void*       arg         = ( void* )       RISC_V::r_user_reg ( A3 );
-                void*       stack_space = ( void* )       RISC_V::r_user_reg ( A4 );
+                thread_t*   handle      = ( thread_t* )   TrapFrame::r_user_reg ( A1 );
+                thread_body t_body      = ( thread_body ) TrapFrame::r_user_reg ( A2 );
+                void*       arg         = ( void* )       TrapFrame::r_user_reg ( A3 );
+                void*       stack_space = ( void* )       TrapFrame::r_user_reg ( A4 );
 
                 if ( handle == nullptr ) {
-                    RISC_V::w_user_reg( A1, ( uint64 ) -1 );
+                    TrapFrame::w_user_reg( A1, ( uint64 ) -1 );
                     break;
                 }
 
                 TCB* tcb = new TCB ( t_body, arg, stack_space );
 
                 if ( tcb == nullptr ) {                          
-                    RISC_V::w_user_reg ( A0, ( uint64 ) -1 );
+                    TrapFrame::w_user_reg ( A0, ( uint64 ) -1 );
                     break;
                 }
 
                 *handle = tcb;
-                RISC_V::w_user_reg ( A0, ( uint64 ) 0 );
+                TrapFrame::w_user_reg ( A0, ( uint64 ) 0 );
                 break;
             }
 
@@ -73,95 +73,95 @@ extern "C" void supervisor_trap_handler () {
             }
 
             case SEM_OPEN: {
-                sem_t*   handle   = ( sem_t* )   RISC_V::r_user_reg ( A1 );
-                unsigned init_val = ( unsigned ) RISC_V::r_user_reg ( A2 );
+                sem_t*   handle   = ( sem_t* )   TrapFrame::r_user_reg ( A1 );
+                unsigned init_val = ( unsigned ) TrapFrame::r_user_reg ( A2 );
 
                 if ( handle == nullptr ) {
-                    RISC_V::w_user_reg( A0, ( uint64 ) -1 );
+                    TrapFrame::w_user_reg( A0, ( uint64 ) -1 );
                     break;
                 }
 
                 KSemaphore* sem = new KSemaphore ( init_val );
 
                 if ( sem == nullptr ) {
-                    RISC_V::w_user_reg ( A0, ( uint64 ) -1 );
+                    TrapFrame::w_user_reg ( A0, ( uint64 ) -1 );
                     break;
                 }
 
                 *handle = sem;
-                RISC_V::w_user_reg ( A0, ( uint64 ) 0 );
+                TrapFrame::w_user_reg ( A0, ( uint64 ) 0 );
                 break;
             }
 
             case SEM_CLOSE: {
-                sem_t handle = ( sem_t ) RISC_V::r_user_reg ( A1 );
+                sem_t handle = ( sem_t ) TrapFrame::r_user_reg ( A1 );
 
                 if ( handle == nullptr ) {
-                    RISC_V::w_user_reg( A0, ( uint64 ) -1 );
+                    TrapFrame::w_user_reg( A0, ( uint64 ) -1 );
                     break;
                 }
 
                 delete handle;
 
-                RISC_V::w_user_reg ( A0, ( uint64 ) 0 );
+                TrapFrame::w_user_reg ( A0, ( uint64 ) 0 );
                 break;
             }
 
             case SEM_WAIT: { 
-                sem_t id = ( sem_t ) RISC_V::r_user_reg ( A1 );
+                sem_t id = ( sem_t ) TrapFrame::r_user_reg ( A1 );
 
                 if ( id == nullptr ) {
-                    RISC_V::w_user_reg( A0, ( uint64 ) -1 );
+                    TrapFrame::w_user_reg( A0, ( uint64 ) -1 );
                     break;
                 }
 
                 int ret = id->wait ();
 
-                RISC_V::w_user_reg ( A0, ( uint64 ) ret );
+                TrapFrame::w_user_reg ( A0, ( uint64 ) ret );
                 break;
             }
 
             case SEM_SIGNAL: {
-                sem_t id = ( sem_t ) RISC_V::r_user_reg ( A1 );
+                sem_t id = ( sem_t ) TrapFrame::r_user_reg ( A1 );
 
                 if ( id == nullptr ) {
-                    RISC_V::w_user_reg( A0, ( uint64 ) -1 );
+                    TrapFrame::w_user_reg( A0, ( uint64 ) -1 );
                     break;
                 }
 
                 int ret = id->signal ();
 
-                RISC_V::w_user_reg ( A0, ( uint64 ) ret );
+                TrapFrame::w_user_reg ( A0, ( uint64 ) ret );
                 break;
             }
 
             case SEM_WAIT_N: {
-                sem_t    id = ( sem_t )    RISC_V::r_user_reg ( A1 );
-                unsigned n  = ( unsigned ) RISC_V::r_user_reg ( A2 );
+                sem_t    id = ( sem_t )    TrapFrame::r_user_reg ( A1 );
+                unsigned n  = ( unsigned ) TrapFrame::r_user_reg ( A2 );
 
                 if ( id == nullptr ) {
-                    RISC_V::w_user_reg( A0, ( uint64 ) -1 );
+                    TrapFrame::w_user_reg( A0, ( uint64 ) -1 );
                     break;
                 }
 
                 int ret = id->wait ( n );
                 
-                RISC_V::w_user_reg ( A0, ( uint64 ) ret );
+                TrapFrame::w_user_reg ( A0, ( uint64 ) ret );
                 break;
             }
 
             case SEM_SIGNAL_N: {
-                sem_t    id = ( sem_t )    RISC_V::r_user_reg ( A1 );
-                unsigned n  = ( unsigned ) RISC_V::r_user_reg ( A2 );
+                sem_t    id = ( sem_t )    TrapFrame::r_user_reg ( A1 );
+                unsigned n  = ( unsigned ) TrapFrame::r_user_reg ( A2 );
 
                 if ( id == nullptr ) {
-                    RISC_V::w_user_reg( A0, ( uint64 ) -1 );
+                    TrapFrame::w_user_reg( A0, ( uint64 ) -1 );
                     break;
                 }
 
                 int ret = id->signal ( n );
 
-                RISC_V::w_user_reg ( A0, ( uint64 ) ret );
+                TrapFrame::w_user_reg ( A0, ( uint64 ) ret );
                 break;
             }
 
@@ -171,12 +171,12 @@ extern "C" void supervisor_trap_handler () {
 
             case CONSOLE_GETC: {
                 char c = __getc ();
-                RISC_V::w_user_reg ( A0, ( uint64 ) c );
+                TrapFrame::w_user_reg ( A0, ( uint64 ) c );
                 break;
             }
 
             case CONSOLE_PUTC: {
-                char c = ( char ) RISC_V::r_user_reg ( A1 );
+                char c = ( char ) TrapFrame::r_user_reg ( A1 );
                 __putc ( c );
                 break;
             }
@@ -186,7 +186,7 @@ extern "C" void supervisor_trap_handler () {
                 print_str ( "Error: unknown syscall\n" );
                 print_str ( "TRAP scause=" ); print_int ( scause );
                 print_str ( " sepc=" );       print_int ( sepc, 16 );
-                print_str ( " stval=" );      print_int ( RISC_V::r_stval () );
+                print_str ( " stval=" );      print_int ( CSR::r_stval () );
                 print_str ( "\n" );
 
                 *( ( uint32* ) 0x100000 ) = 0x5555; // halt the emulator
@@ -198,19 +198,19 @@ extern "C" void supervisor_trap_handler () {
         print_str ( "Error: unknown trap\n" );
         print_str ( "TRAP scause=" ); print_int ( scause );
         print_str ( " sepc=" );       print_int ( sepc, 16 );
-        print_str ( " stval=" );      print_int ( RISC_V::r_stval () );
+        print_str ( " stval=" );      print_int ( CSR::r_stval () );
         print_str ( "\n" );
 
         *( ( uint32* ) 0x100000 ) = 0x5555; // halt the emulator
     }
 
-    RISC_V::w_sepc     ( sepc );
-    RISC_V::w_sstatus  ( sstatus );
+    CSR::w_sepc     ( sepc );
+    CSR::w_sstatus  ( sstatus );
 }
 
 extern "C" void timer_interrupt_handler () {
     // clear timer interrupt
-    RISC_V::mc_sip ( RISC_V::SIP_SSIP );
+    CSR::mc_sip ( CSR::SIP_SSIP );
 }
 
 extern "C" void console_interrupt_handler () {
